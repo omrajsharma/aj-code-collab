768abcd3b927925643c1167df09a1f5759de87fc
om-weye <omraj.sharma@wheelseye.com>
2025-01-22 22:24:25 +0530

    realtime code added

--- src/Actions.jsx
+++ src/Actions.jsx
@@ -2,7 +2,7 @@ export const ACTIONS = {
     JOIN: 'join',
     JOINED: 'joined',
     DISCONNECTED: 'disconnected',
-    CODE_CHANGE: 'code-change',
+    CODE_CHANGE: 'conde-change',
     SYNC_CODE: 'sync-code',
     LEAVE: 'leave'
 }
--- src/components/EditorPage.jsx
+++ src/components/EditorPage.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useRef, useState } from 'react'
-import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import { Navigate, useLocation, useNavigate, useParams } from 'react-router-dom';
 import { initSocket } from '../Socket';
 import toast from 'react-hot-toast';
-import ACTIONS from '../Actions'
+import {ACTIONS} from '../Actions'
+import Editor from './Editor';
 
 const LANGAUGES = [
   "python3",
@@ -43,24 +44,125 @@ const EditorPage = () => {
       socketRef.current = await initSocket();
       socketRef.current.on('connect_error', err => handleErrors(err))
       socketRef.current.on('connect_failed', err => handleErrors(err))
-      
-      console.log('inside useeffect');
-
       const handleErrors = err => {
         console.log('Error', err);
         toast.error('Socket connection failed, try again later')
         navigate('/');
       }
 
+      // emit - joining the room
       socketRef.current.emit(ACTIONS.JOIN, {
         roomId,
         username: Location.state?.username
       })
+
+      // on - if some user join
+      socketRef.current.on(
+        ACTIONS.JOINED,
+        ({clients, username, socketId}) => {
+          if (username !== Location.state?.username) {
+            toast.success(`${username} joined the room`)
+          }
+          setClients(clients);
+          socketRef.current.emit(ACTIONS.SYNC_CODE, {
+            code: codeRef.current,
+            socketId
+          })
+        }
+      )
+
+      // on - if some user disconnect
+      socketRef.current.on(ACTIONS.DISCONNECTED, 
+        ({socketId, username}) => {
+          toast.success(`${username} left the room`)
+          setClients((prev) => {
+            return prev.filter(client => client.socketId != socketId)
+          })
+        } 
+      )
+    }
+
+    init();
+
+    return () => {
+      socketRef.current && socketRef.current.disconnect();
+      // socketRef.current.off(ACTIONS.JOINED);
+      // socketRef.current.off(ACTIONS.DISCONNECTED);
     }
   }, [])
 
+  if (!Location.state) {
+    return <Navigate to="/" />
+  }
+
+  const copyRoomId = async() => {
+    try {
+      await navigator.clipboard.writeText(roomId);
+      toast.success("Room ID is copied");
+    } catch (error) {
+      console.log(error);
+      toast.error("Unable to copy the room ID")
+    }
+  }
+
+  const leaveRoom = async () => {
+    navigate('/')
+  }
+
+  const runCode = async () => {
+    // todo: add the logic
+  }
+
+  const toggleCompileWindow = () => {
+    setIsCompileWindowOpen(!isCompileWindowOpen);
+  }
+  
+
   return (
-    <div>EditorPage</div>
+    <div>
+      <div>
+        <div>
+          <img src="https://acciojob.com/src/Navbar/logo.svg" alt="" />
+          
+          <hr />
+
+          <div>
+            <h3>Members</h3>
+            <div style={{backgroundColor: "#23486A", padding: "8px 16px"}}>
+              {clients.map(client => <p>🟢 {client.username}</p> )}
+            </div>
+          </div>
+
+          <hr />
+
+          <div>
+            <button onClick={copyRoomId}>Copy Room ID</button>
+            <button onClick={leaveRoom}>Leave Room</button>
+          </div>
+
+          <hr />
+
+          <div>
+            <div>
+              <select
+                value={selectedLanguage}
+                onChange={e => setSelectedLanguage(e.target.value)}
+              >
+                {LANGAUGES.map(lang => <option key={lang} value={lang}>{lang}</option>)}
+              </select>
+            </div>
+          </div>
+
+          <Editor 
+            socketRef={socketRef}
+            roomId={roomId}
+            onCodeChange={code => {
+              codeRef.current = code;
+            }}
+          />
+        </div>
+      </div>
+    </div>
   )
 }
 
